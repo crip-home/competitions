ca4014099dca270f06cd8f03ce4e032e99955559
Igors Krasjukovs <tahq69@gmail.com>
2017-01-22 18:29:03 +0200

    Message model updated;

--- app/Message.php
+++ app/Message.php
@@ -1,6 +1,7 @@
 <?php namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Class Message
@@ -8,5 +9,49 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Message extends Model
 {
-    //
+    use HasAuditTrait;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'messages';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'body',             // text
+        'created_by',       // int(10) UNSIGNED
+        'created_by_name',  // varchar(255)
+        'from_id',          // int(10) UNSIGNED NULL
+        'from_name',        // varchar(255)
+        'importance_level', // smallint(5) 10
+        'is_read',          // tinyint(1) 0
+        'subject',          // varchar(255)
+        'to_id',            // int(10) UNSIGNED
+        'to_name',          // varchar(255)
+        'type',             // varchar(20)
+        'updated_by',       // int(10) UNSIGNED NULL
+        'updated_by_name',  // varchar(255) ''
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'from_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function recipient()
+    {
+        return $this->belongsTo(User::class, 'to_id', 'id');
+    }
 }
